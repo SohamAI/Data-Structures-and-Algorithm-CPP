05b1a9a1d9aa52c41295554ae9ada2159da8f2b6
Soham <soham.mhatre.ai@gmail.com>
2024-06-17 19:43:27 +0530

    first Program

---
+++ first.cpp
@@ -0,0 +1,14 @@
+/**
+ Name : Soham K Mhatre
+ Date : 17-06-2024
+ Topic : To Understand the Basics of C++ (Revision)
+ */
+
+#include<iostream> 
+using namespace std;
+
+int main(){
+
+    cout<<"Hello World"<<endl;
+    return 0;
+}
