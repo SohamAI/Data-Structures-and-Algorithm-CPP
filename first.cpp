9b488db1c98e98d94212c094b9514eaad55a92d4
Soham <soham.mhatre.ai@gmail.com>
2024-06-17 19:54:08 +0530

    first program explained

--- first.cpp
+++ first.cpp
@@ -4,11 +4,12 @@
  Topic : To Understand the Basics of C++ (Revision)
  */
 
-#include<iostream> 
-using namespace std;
+#include<iostream> // include the input-output stream Library
+using namespace std; // use the standard namespace which includes Objects like cin , cout , etc
 
-int main(){
+int main(){ //--> the program in C++ starts From the main Function
 
-    cout<<"Hello World"<<endl;
-    return 0;
+    // --> First Program : 
+    cout<<"Hello World"<<endl; // --> using cout we can output the results in terminal
+    return 0; //--> returns the 0 from the main function indicating the program was successfully executed
 }
